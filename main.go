32af246025e1197d60f5df0dacce1f9ca3d87718
Pavel Kus <pavel.kus@gmail.com>
2024-03-19 18:47:42 +0100

    create and read project

--- main.go
+++ main.go
@@ -1,13 +1,60 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Project struct {
+	ProjectID int
+}
+
+var (
+	projects = []*Project{}
+	mux      sync.Mutex
+)
+
+func createProject(c *gin.Context) {
+	mux.Lock()
+	defer mux.Unlock()
+
+	projectID := len(projects) + 1
+	project := &Project{ProjectID: projectID}
+	projects = append(projects, project)
+
+	c.JSON(200, gin.H{
+		"projectID": projectID,
+	})
+}
+
+func readProject(c *gin.Context) {
+	projectID, _ := strconv.Atoi(c.Param("id"))
+
+	if projectID <= 0 || projectID > len(projects) {
+		c.JSON(404, gin.H{"error": "Project not found"})
+		return
+	}
+
+	project := projects[projectID-1]
+
+	c.JSON(200, project)
+}
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	eqh := r.Group("/eqh")
+
+	eqh.POST("/createProject", createProject)
+	eqh.GET("/:id/readProject", readProject)
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
